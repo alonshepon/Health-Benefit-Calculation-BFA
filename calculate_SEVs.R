5c4a13954bb04631d1f83ba89dbf5224ff90226d
alonshepon <63227352+alonshepon@users.noreply.github.com>
2020-11-26 20:34:15 +0200

    change location of folder

--- calculate_SEVs.R
+++
@@ -1,7 +0,0 @@
-
-#calculate SEV (summary exposure values) for each sex-age-location group for meat and seafood consumption
-
-
-
-#combine RR values and intake per age-sex location
-
---
+++ code/calculate_SEVs.R
@@ -0,0 +1,10 @@
+
+#calculate SEV (summary exposure values) for each sex-age-location group for meat and seafood consumption
+
+
+
+#step 1: create a function that calculates SEV per age-sex-location and micronutrient/meat/omega n-3
+#SEV = integrate(RR*Intake)/RRmax
+
+
+
