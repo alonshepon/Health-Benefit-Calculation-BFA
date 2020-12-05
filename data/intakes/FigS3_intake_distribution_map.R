d884f8933d05ab8edc21968c51283a9c86511318
Chris Free <cfree@iMac-4.local>
2020-12-04 16:39:15 -0800

    attmept at nutrient intake key

--- data/intakes/FigS3_intake_distribution_map.R
+++ data/intakes/FigS3_intake_distribution_map.R
@@ -23,7 +23,7 @@ world <- rnaturalearth::ne_countries("small", returnclass="sf")
 # Intake data
 intake_cntrys <- c("China", "Laos", "Mexico", "Philippines", "Uganda", "United States of America", "Zambia", "Italy")
 world_intake <- world %>% 
-  filter(geounit %in% intake_cntrys)
+  filter(geounit %in% intake_cntrys) 
 world_intake_centroid <- world_intake %>% 
   sf::st_centroid()
 
@@ -96,5 +96,72 @@ g
 # Export plot
 ggsave(g, filename=file.path(plotdir, "FigureS3_intake_data_un_subregion.png"), 
        width=6.5, height=4, units="in", dpi=600)
+
+
+
+# Build intake map
+################################################################################
+
+# World key
+intake_key_df <- world %>% 
+  sf::st_drop_geometry() %>% 
+  select(geounit, gu_a3, subregion) %>% 
+  rename(country=geounit, iso3=gu_a3) %>% 
+  # Classify
+  mutate(intake_group=recode(subregion,
+                             "Antarctica"="N/A", 
+                             "Australia and New Zealand"="United States", 
+                             "Caribbean"="Mexico", 
+                             "Central America"="Mexico",  
+                             "Central Asia"="China",  
+                             "Eastern Africa"="Uganda & Zambia", 
+                             "Eastern Asia"="China", 
+                             "Eastern Europe"="Italy", 
+                             "Melanesia"="Laos & Philippines",  
+                             "Middle Africa"="Uganda & Zambia", 
+                             "Northern Africa"="Uganda & Zambia",  
+                             "Northern America"="United States", 
+                             "Northern Europe"="Italy",  
+                             "Seven seas (open ocean)"="N/A",  
+                             "South America"="Mexico", 
+                             "South-Eastern Asia"="Laos & Philippines", 
+                             "Southern Africa"="Uganda & Zambia",  
+                             "Southern Asia"="China",  
+                             "Southern Europe"="Italy",  
+                             "Western Africa"="Uganda & Zambia",  
+                             "Western Asia"="China", 
+                             "Western Europe"="Italy"))
+
+# Add to world and map
+intake_key_sf <- world %>% 
+  select(gu_a3) %>% 
+  rename(iso3=gu_a3) %>% 
+  left_join(intake_key_df)
+
+g <- ggplot(intake_key_sf) +
+  geom_sf(mapping=aes(fill=intake_group), color="grey30", lwd=0.2) +
+  # Add points for intake countries
+  geom_sf(data=world_intake_centroid, color="black", size=2) +
+  # Labels
+  labs(title="Intake group") +
+  # Legend
+  scale_fill_discrete(name="Intake group") +
+  # Theme
+  theme_bw() +
+  theme(axis.text=element_text(size=6),
+        axis.title=element_text(size=8),
+        legend.text=element_text(size=6),
+        legend.title=element_text(size=8),
+        strip.text=element_text(size=8),
+        plot.title=element_text(size=10),
+        # Gridlines
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"))
+g
+
+ggsave(g, filename=file.path(plotdir, "FigureS3_intake_data_map_assignment.png"), 
+       width=6.5, height=3, units="in", dpi=600)
 
 
