21a599f8710898fa7957112f2843391151082943
alonshepon <63227352+alonshepon@users.noreply.github.com>
2020-12-13 14:23:22 +0200

    rr

--- code/Build_intake_distributions.R
+++
@@ -1,9 +0,0 @@
-#This script combines the COSOMO AgiLink with the SPADE output to produce distributions of intakes
-
-#step 1: read COSOMO AgiLink model mean values
-
-#step 2: in case the model shows an average value for country, disagrregate each country to derive age-sex groups mean values per location
-
-#step 3: read Simone's database distribution
-
-#step 4: Use Method of Moments and fits to build means and distribution for each of the age-sex-location-meat/omega n-3/micronutrient group
