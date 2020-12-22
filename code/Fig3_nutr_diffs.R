65f64878b320cc386e9328b6f7beac109e74828c
Chris Free <cfree@iMac-4.local>
2020-12-21 16:48:58 -0800

    updated fig 3 code

--- code/Fig3_nutr_diffs.R
+++ code/Fig3_nutr_diffs.R
@@ -11,85 +11,75 @@ library(tidyverse)
 library(countrycode)
 
 # Directories
-outputdir <- "data/cosimo/processed"
+outputdir <- "data/cosimo/old/processed"
 plotdir <- "figures"
 
 # Read data
-data_orig <- readRDS(file.path(outputdir, "COSIMO_2010_2030_nutr_by_scenario_cntry_food.Rds"))
+data_orig <- readRDS(file.path(outputdir, "COSIMO_2010_2030_perc_nutr_diff_by_food.Rds"))
 
 # World
-world <- rnaturalearth::ne_countries("small", returnclass = "sf")
+world <- rnaturalearth::ne_countries(scale="small", returnclass = "sf")
 
 
-# Format data
+# Plot data
 ################################################################################
 
-# Format data
+
+# Nutrients to plot
+nutrients_plot <- c("Iron", "Zinc", "Calcium", 
+                    "Vitamin A", "Vitamin B-12", "Omega-3 fatty acids")
+
+# Data
 data <- data_orig %>% 
-  filter(food=="Total Diet")
+  filter(year==2030 & food=="Total Diet" & nutrient %in% nutrients_plot)
 
-# Read data
+# Expand data to match map countries
+data_expanded <- world %>% 
+  sf::st_drop_geometry() %>% 
+  # Reduce
+  select(gu_a3) %>% 
+  # Add nutrient info
+  left_join(data, by=c("gu_a3"="country_iso3")) %>% 
+  # Expand
+  complete(gu_a3, nutrient) %>% 
+  # Remove missing data
+  filter(!is.na(nutrient))
+
+# Add expanded data to SF
+data_sf <- world %>% 
+  left_join(data_expanded, by="gu_a3")
+
+
+# Plot data
 ################################################################################
 
-# Base theme
-base_theme <-  theme(axis.text=element_text(size=6),
-                     axis.title=element_text(size=8),
-                     legend.text=element_text(size=6),
-                     legend.title=element_text(size=8),
-                     strip.text=element_text(size=8),
-                     plot.title=element_text(size=9),
-                     # Gridlines
-                     panel.grid.major = element_blank(), 
-                     panel.grid.minor = element_blank(),
-                     panel.background = element_blank(), 
-                     axis.line = element_line(colour = "black"))
+# Plot data
+g <- ggplot(data_sf) +
+  facet_wrap(~nutrient, ncol=3) +
+  geom_sf(mapping=aes(fill=perc_diff), lwd=0.1, color="grey30") +
+  # Legend
+  scale_fill_gradient2(name="% difference in 2030 intakes\n(high vs. base)", midpoint=0, low="darkred", high="navy", mid="white", na.value = "grey80") +
+  guides(fill = guide_colorbar(ticks.colour = "black", frame.colour = "black")) +
+  # Crop out Antarctica
+  coord_sf(y=c(-55, NA)) +
+  # Theme
+  theme_bw() +
+  theme(legend.position = "bottom",
+        axis.text=element_blank(),
+        axis.title=element_blank(),
+        legend.text=element_text(size=6),
+        legend.title=element_text(size=8),
+        strip.text=element_text(size=6),
+        plot.title=element_text(size=8),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(), 
+        axis.line = element_line(colour = "black"))
+g
 
+# Export plot
+ggsave(g, filename=file.path(plotdir, "Fig3_nutrient_diffs.png"), 
+       width=6.5, height=3, units="in", dpi=600)
 
-
-
-
-
-
-
-# Plot map
-plot_map <- function(nutrient){
-  
-  # Format data
-  nutr_do <- nutrient
-  data_sf <- world %>% 
-    left_join(data %>% filter(nutrient==nutr_do), by=c("gu_a3"="iso3"))
-  
-  # Plot
-  g <- ggplot(data_sf) +
-    geom_sf(mapping=aes(fill=value_pdiff), lwd=0.2) +
-    # Crop out Antarctica
-    coord_sf(y=c(-55, NA)) +
-    # Legend and labels
-    labs(title=nutr_do) +
-    scale_fill_gradient2(name="% change in intake\n(high relative to base)", 
-                         low="darkred", high="navy", mid="white", midpoint=0) +
-    guides(fill = guide_colorbar(ticks.colour = "black", frame.colour = "black", barwidth = 0.8, barheight = 3)) +
-    # Theme
-    theme_bw() + base_theme +
-    theme(legend.position=c(0.11,0.37),
-          axis.text = element_blank(),
-          axis.title=element_blank(), 
-          legend.background = element_rect(fill=alpha('blue', 0)))
-  g
-  
-}
-
-# Plot maps
-zinc <- plot_map("Zinc")
-calcium <- plot_map("Calcium")
-vitaminA <- plot_map("Vitamin A")
-iron <- plot_map("Iron")
-omegas <- plot_map("Omega-3 fatty acids")
-zinc <- plot_map("Zinc")
-vitaminB12 <- plot_map("Vitamin B-12")
-
-# Merge
-
-# Export
 
 
